a64af627c4b9e19adfd4a27adefc23dc2ea6ab54
Fan Wang <496100630@qq.com>
2015-12-31 15:23:50 +0800

    6

--- mylist.h
+++ mylist.h
@@ -24,7 +24,16 @@ template<class T>
 			a->next = b;
 			b->last = a;
 		}
-		
+		Node<T>* copy(Node<T> *p, my_List<T> *a){
+			Node<T> *p1 = a->head;
+			while(p1->next != NULL){
+				p->next = new Node<T>(p1->next->v);
+				f(p, p->next);
+				p = p->next;
+				p1 = p1->next;
+			}
+			return p;
+		}
 		my_List()
 		{
 			end = head = new Node<T>;	
@@ -34,14 +43,7 @@ template<class T>
 		my_List(my_List<T> *a)
 		{
 			head = new Node<T>;
-			Node<T> *p = head, *p1 = a->head;
-			while(p1->next != NULL){
-				p->next = new Node<T>(p1->next->v);
-				f(p, p->next);
-				p = p->next;
-				p1 = p1->next;
-			}
-			end = p;
+			end = copy(head, a);
 			size = a->size;
 		}
 		void del(Node<T> *p){
@@ -49,8 +51,9 @@ template<class T>
 			{
 				end = p->last;
 				end->next = NULL;
+			}else{
+				f(p->last, p->next);
 			}
-			f(p->last, p->next);
 			delete(p);
 			size--;
 		}
@@ -66,8 +69,11 @@ template<class T>
 			size++;
 		}
 
-		void add(my_List<T> *ls, int t){
-			if(!t){
+/*		void add(my_List<T> *ls, int t){
+			if(size == 0){
+				head->next = ls->head->next;
+				end = ls->end;
+			}else if(!t){
 				f(ls->end, head->next);
 				f(head, ls->head->next);
 			}else{
@@ -75,7 +81,27 @@ template<class T>
 				end = ls->end;
 			}
 			size += ls->size;
+		}*/
+		void add(my_List<T> *ls, int t){
+			if(size == 0){
+				end = copy(head, ls);
+			}else if(!t){
+				Node<T> *p = head->next;
+				(end = copy(head, ls))->next = p;
+			}else{
+				end = copy(end, ls);
+			}
+			size += ls->size;
 		}
 	};
-
+void delete_list(my_List<string> *p){
+	Node<string> *a = p->head, *b = p->head->next;
+	delete(a);
+	while(b != NULL){
+		a = b;
+		b = a->next;
+		delete(a);
+	}
+	delete(p->end);
+}
 #endif
--- parse.cc
+++ parse.cc
@@ -49,11 +49,17 @@ struct CFG
 		VList ans;
 		VList &ret = exp[nt];
 		for(int i = 0; i < ret.size(); i++){
-			my_List<string> *a = new my_List<string>(ls);
+			my_List<string> *a = new my_List<string>(&ls);
+		
 			a->del(a->head->next);
+		
 			a->add(&ret[i], 0);
+		
 			ans.push_back(*a);
-		}
+		
+		}	
+		VList &a = exp[nt];
+
 		return ans;
 	}
 
@@ -64,51 +70,46 @@ void CFG::emilite(string& nt){
 	VList ret;
 	bool *isA = new bool[vc.size()];
 	bool ok = false;
-	cout  <<" wer sdw" << endl; 
 	for(int i = 0; i < vc.size(); i++){
-		cout << " vvvv" << endl;
 		if(vc[i].head->next->v == nt)
 			isA[i] = true;
 		else
 			isA[i] = false;
 		ok |= isA[i];
 	}
-	cout << ok << " gh" << endl;
 	if(!ok) return ;
 	string A1 = nt + "!";
 	newNT.push_back(A1);
-	cout << "696969"  << endl; 
 	VList vls;
 	for(int i = 0; i < vc.size(); i++){
 		if(!isA[i]){
 			my_List<string> ls;
-			cout << "22221" << endl;
+		
 			ls.add(&vc[i], 1);
-			cout << "22222" << endl;
+			
 			ls.add(new Node<string>(A1), 1);
-			cout << "564654" << endl;
+			
 			vls.push_back(ls);
+	           
 		}else{
-			cout << "111111" << endl;
 		    vc[i].del(vc[i].head->next);
-			cout << "33333" << endl;
 			my_List<string> *p = new my_List<string>(&vc[i]);
-			cout << "22222" << endl;
+		//	delete_list(&vc[i]);
 			p->add(new Node<string>(A1), 1);
-			exp[A1].push_back(*p); 
-			cout << " bbbdffg " << endl;
+			exp[A1].push_back(*p);
 		}
 	}
-	Node<string> *test = exp[A1][0].head->next;
-	cout << (test->v) /*<< "  " << (test->next->v) */<< "bvbbvvv" << endl;
 	exp[A1].push_back(new my_List<string>);
 	exp[nt] = vls;
+
 }
 
 void CFG::replace()
 {
 	for(int i = 0; i < NT.size(); i++){
 		emilite(NT[i]);
+		VList &a = exp[NT[0]];
+
 		for(int j = i + 1; j < NT.size(); j++){
 			vector<my_List<string> > &vc = exp[NT[j]];
 			VList vls;
@@ -123,9 +124,8 @@ void CFG::replace()
 				}
 			}
 			vc = vls;
-		}		
+		}	
 	}
-	cout << exp[NT[0]][0].head->next->v <<" !!!"<< endl;
 	NT.insert(NT.end(), newNT.begin(), newNT.end());
 }
 
@@ -136,18 +136,13 @@ void CFG::analysis_P(){      // 把一个表达式分解为： 一a个非终结�
 	for(int i = 0; i < (int)NT.size(); i++){
 		for(int j = 0; j < mp[NT[i]].size(); j++){
 			my_List<string> *lst = new my_List<string>;
-			cout << "qwewq" << endl;
 			for(int k = 0; k < mp[NT[i]][j].size(); k++)
 			{
 				lst->add(new Node<string>(mp[NT[i]][j][k]), 1);
-				cout << "zxc" << endl;
 			}
-			
 			exp[NT[i]].push_back(*lst);
 		}
-	//	cout << (exp[NT[0]][0].head->next == NULL) << "~~~~~~~~~" << (exp[NT[0]][0].head->next->next->next == NULL)<< " bvbb" << endl;
 	}
-	cout << "qweqwe" << endl;	
 }
 
 void CFG::split(const string& str){
@@ -167,17 +162,14 @@ void CFG::split(const string& str){
 		{
 			if( comp(NT[j], str, i) )
 			{
-				//	cout << "dsfgwer" << endl;
 				mp[NT[mark]][size].push_back(NT[j]);
 				ok = true;
 				i += NT[j].size() - 1;
 			}	
 		}
-	//	cout << "sdfwer" << endl;
 		for(int j = 0; !ok && j < (int)T.size(); j++)
 		{
 			if( comp(T[j], str, i) ){
-			//	cout << "sdfee" << endl;
 				mp[NT[mark]][size].push_back(T[j] + "`");
 				ok = true;
 				i += T[j].size() - 1;
@@ -191,13 +183,14 @@ int main()
 {
 //	init();
 	cfg->analysis_P();
-	cout << "sdfsdf" << endl;
 	cfg->replace();
-//	cout <<cfg->NT.size() <<  " sdfwqer" << endl;
+	
+	cout << " ~~~~~~~~~~~~~~~~~~~~ " << endl;
 	for(int i = 0; i < cfg->NT.size(); i++){
 		VList &ret = cfg->exp[cfg->NT[i]];
 		for(int j = 0; j < ret.size(); j++){
 			Node<string> *p = ret[j].head;
+			cout << cfg->NT[i] << "->";
 			while(p->next != NULL){
 				cout << p->next->v << " ";
 				p = p->next;
